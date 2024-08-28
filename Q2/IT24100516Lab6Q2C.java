377990d829ddfd11821b2413bac76b1053b7b2fa
IT24100516 <it24100516@my.sliit.lk>
2024-08-28 11:58:13 +0530

    IT24100516Lab6Q2C.java

---
+++ Q2/IT24100516Lab6Q2C.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class IT24100516Lab6Q2C
+{
+	public static void main(String[]args)
+	{
+	Scanner input=new Scanner(System.in);
+
+	int sum ;
+	double average;
+	int num1 , num2, num3 , num4 , num5 , num6 , num7 , num8 , num9 , num10 ;
+	System.out.println("Please enter 10 numbers: ");
+
+	System.out.print("Enter number1: ");
+	num1=input.nextInt();
+	
+	System.out.print("Enter number2: ");
+	num2=input.nextInt();
+
+	System.out.print("Enter number3: ");
+	num3=input.nextInt();
+	
+	System.out.print("Enter number4: ");
+	num4=input.nextInt();
+	
+	System.out.print("Enter number5: ");
+	num5=input.nextInt();
+
+	System.out.print("Enter number6: ");
+	num6=input.nextInt();
+
+	System.out.print("Enter number7: ");
+	num7=input.nextInt();
+
+	System.out.print("Enter number8: ");
+	num8=input.nextInt();
+
+	System.out.print("Enter number9: ");
+	num9=input.nextInt();
+
+	System.out.print("Enter number10: ");
+	num10=input.nextInt();
+
+	sum=num1+num2+num3+num4+num5+num6+num7+num8+num9+num10;
+	average=sum/10.0;
+
+	System.out.print("The numbers you entered are: ");
+	System.out.print(num1 + " " + num2 + " " + num3 + " " + num4 + " " + num5 + " " + num6 + " " + num7 + " " + num8 + " " + num9 + " " + num10 );
+
+	System.out.print("\nThe sum: "+sum);
+	System.out.print("\nThe average: "+average);
+
+	return;
+	}
+}
